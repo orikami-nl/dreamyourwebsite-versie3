97c684d4d300f20b2bc1e11a434a2afe07624a3f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-11-08 16:46:40 +0100

    comments fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -51,7 +51,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-		    CommentMail.create(:body => @comment.body, :name => @comment.name, :email => @comment.email, :partner_email => (@post.partner.name.match(/\w+/).to_s.downcase + "@dreamyourweb.nl"), :post_title => @post.title).deliver
+		    CommentMail.new(:body => @comment.body, :name => @comment.name, :email => @comment.email, :partner_email => (@post.partner.name.match(/\w+/).to_s.downcase + "@dreamyourweb.nl"), :post_title => @post.title).deliver
         format.html { redirect_to partner_post_path(@partner, @post), notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
