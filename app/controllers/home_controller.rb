a4a99955683ea3828bcd3567bd1ed23b5a1b84dc
Andres Lamont <alamont@gmail.com>
2011-11-09 22:54:40 +0100

    portfolio and about headers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,7 @@ class HomeController < ApplicationController
     end
     @posts = Post.last(2)
     @project = Project.last
-    @lasttweet = Twitter.user_timeline("invite2eat").first.text  
+    @lasttweet = Twitter.user_timeline("dreamyourweb").first.text  
     @lasttweet = @lasttweet.strip.gsub(/(http:\/\/\S+)/) {|s| '<a href=' + s + '>' + s + '</a>'}
     @lasttweet = @lasttweet.strip.gsub(/(#\w+)/) {|s| '<a href=http://twitter.com/#!/search?q=%23' + s.gsub(/#/,"") + '>' + s + '</a>'}
     @lasttweet = @lasttweet.strip.gsub(/(@\w+)/) {|s| '<a href=http://twitter.com/#!/' + s.gsub(/@/,"") + '>' + s + '</a>'}
