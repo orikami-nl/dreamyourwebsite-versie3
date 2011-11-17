d095f4c0e69fa4a0a72f3a5cb3c26d75487a2020
Andres Lamont <alamont@gmail.com>
2011-11-17 07:59:13 +0100

    Shadow for blog image. Margin on blog selection images.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,12 @@ class ProductsController < ApplicationController
 manageable_content_for :title, :body, :sidebar, :webdevelopment, :concept_development, :mobile, :web_excerpt, :concept_excerpt, :mobile_excerpt, :layout => "sidebar_layout"
 	layout "sidebar_layout"
 
+	before_filter :parse
+
+	def parse
+		# @bla = manageable_layout_content_for :sidebar
+	end
+
 	def index
 	end
 
