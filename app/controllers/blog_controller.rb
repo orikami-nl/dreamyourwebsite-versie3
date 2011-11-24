7d2b447cae55b89d3a2ea4196276a942c37cd5cc
Andres Lamont <alamont@gmail.com>
2011-11-24 13:43:27 +0100

    Fixed about admin buttons

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,20 +1,31 @@
 class BlogController < ApplicationController
-	manageable_content_for :body, :title, :layout => "sidebar_layout"
+  manageable_content_for :body, :title, :layout => "sidebar_layout"
 
-	layout "sidebar_layout"
-	before_filter :tag_cloud
-	caches_action :index, :tag_cloud
+  layout "sidebar_layout"
+  before_filter :tag_cloud
+  caches_action :index, :cache_path => :index_cache_path.to_proc
+  cache_sweeper :post_sweeper
 
-	def tag_cloud
-		@tags = Post.tag_counts_on(:tags)
-	end
+  def tag_cloud
+  	@tags = Post.tag_counts_on(:tags)
+  end
 
   def index
-		if !params[:tag].nil?
-			@posts = Post.tagged_with(params[:tag]).page(params[:page]).per(5)
-		else
-			@posts = Post.page(params[:page]).per(5)
-		end
-		@partners = Partner.all
+  	if !params[:tag].nil?
+  		@posts = Post.tagged_with(params[:tag]).page(params[:page]).per(5)
+  	else
+  		@posts = Post.page(params[:page]).per(5)
+  	end
+  	@partners = Partner.all
+  end
+
+  # private
+
+  def index_cache_path
+  	if admin_signed_in?
+  		'/admin/blog'
+  	else
+  		'/public/blog'
+  	end
   end
 end
