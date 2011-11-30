a1dc18e05678658d822d4b144d59ccfaf3956da8
Andres Lamont <alamont@gmail.com>
2011-11-30 19:57:55 +0100

    'Changed sub-navigation on frontpage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -76,6 +76,12 @@ $(document).ready(function(){
 
     });
 
+    $('.fade').hover(function() { 
+    $(this).stop().animate({"opacity": 1}); 
+    },function() { 
+        $(this).stop().animate({"opacity": 0}); 
+    });
+
   });
 
 
