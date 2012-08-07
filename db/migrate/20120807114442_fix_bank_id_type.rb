98b6bb0b31021ba92c6f4c21cbac23137e3d5e6a
Andres Lamont <alamont@gmail.com>
2012-08-07 14:06:37 +0200

    Default value bank_id

--- db/migrate/20120807114442_fix_bank_id_type.rb
+++ db/migrate/20120807114442_fix_bank_id_type.rb
@@ -1,5 +1,5 @@
 class FixBankIdType < ActiveRecord::Migration
   def change
-  	change_column :dashboard_bank_balances, :bank_id, :integer
+  	change_column :dashboard_bank_balances, :bank_id, :integer, :default => 0
   end
 end
