c0e0a2bb90efc5baaee7da625c6368f28ef686c7
Thijs van de Laar <thijsvdlaar@hotmail.com>
2013-03-19 17:54:16 +0100

    xml and default title

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,7 +1,7 @@
 class SitemapController < ApplicationController
   def index
      @not_so_static_pages = [blog_url, projects_url]
-     @static_pages = [root_url, about_url, contact_url, products_url, flow_url]
+     @static_pages = [root_url, about_url, contact_url, products_url, flow_url, data_analysis_url, data_visualization_url, ruby_on_rails_url, survey_app_url, machine_learning_url, lean_management_url]
      @really_static_pages = [terms_and_conditions_url, disclaimer_url]
      @posts = Post.all
      @projects = Project.all
